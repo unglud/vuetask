fed46009c867118e368c20e2a840804402a8d081
Aleksandr Matrosov <unglud@gmail.com>
2021-04-30 18:50:40 +0200

    toggle form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button @toggle-add-task="$emit('toggle-add-task')" text="Add Task" color="green" />
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close':'Add Task'" :color="showAddTask ? 'red':'green'" />
   </header>
 </template>
 
@@ -13,7 +13,10 @@ export default {
   components:{
     Button
   },
-  props: ['title']
+  props:{
+    title: String,
+    showAddTask: Boolean
+  }
 }
 </script>
 
