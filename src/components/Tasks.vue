376ec4d313b7f652ed13490172b8baf91c8c7488
Aleksandr Matrosov <unglud@gmail.com>
2021-04-30 15:49:13 +0200

    toggle

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,7 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
-    <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+    <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)"
+          :task="task"/>
   </div>
 </template>
 
@@ -9,11 +10,11 @@ import Task from './Task';
 
 export default {
   name: 'Tasks',
-  components:{
+  components: {
     Task
   },
-  props:{
-    tasks:[]
+  props: {
+    tasks: []
   }
 }
 </script>
