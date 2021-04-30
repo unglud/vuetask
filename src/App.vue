73994bc980047b80a91c6ead5b1fe49377dcd4ac
Aleksandr Matrosov <unglud@gmail.com>
2021-04-30 15:24:18 +0200

    tasks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
+    <Header title="Task Tracker"/>
+    <Tasks :tasks="tasks" />
   </div>
 
 
@@ -8,25 +9,45 @@
 
 <script>
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  created () {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors',
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      }
+    ]
   }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: 'Poppins', sans-serif;
 }
+
 .container {
   max-width: 500px;
   margin: 30px auto;
@@ -36,6 +57,7 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
+
 .btn {
   display: inline-block;
   background: #000;
@@ -49,12 +71,15 @@ body {
   font-size: 15px;
   font-family: inherit;
 }
+
 .btn:focus {
   outline: none;
 }
+
 .btn:active {
   transform: scale(0.98);
 }
+
 .btn-block {
   display: block;
   width: 100%;
---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task';
+
+export default {
+  name: 'Tasks',
+  components:{
+    Task
+  },
+  props:{
+    tasks:[]
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
