b52523e2487720d493968532eadb4984b8bd4bba
Aleksandr Matrosov <unglud@gmail.com>
2021-04-30 18:46:54 +0200

    toggle form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker"/>
-    <AddTask @add-task="addTask" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker"/>
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask"/>
+    </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
@@ -20,10 +22,14 @@ export default {
   },
   data () {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
     deleteTask (id) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
     },
